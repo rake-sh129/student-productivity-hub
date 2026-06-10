32cc32da3b73439939507f74b474b7bd3656ae1b
Rakesh Kumar <rr1017220@gmail.com>
2026-06-10 14:26:16 +0530

    Add Navbar component and navbar styles

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <header className='navbar'  >
+        <h2>Student Productivity Hub</h2>
+    </header>
+  )
+}
+
+export default Navbar
